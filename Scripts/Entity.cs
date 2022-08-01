89b4a896a29afbef726f1f0ec889982a91d2b2cd
Jon Gao <jonjon.liho@gmail.com>
2022-08-01 20:22:19 +0800

    update namespace

--- Scripts/Entity.cs
+++ Scripts/Entity.cs
@@ -5,138 +5,141 @@ using UnityEngine;
 using UnityEditor;
 #endif
 
-public class EntityState : KeyedObject<bool>
+namespace HacTac
 {
-    protected string m_guid;
-    protected Vector3 m_worldPosition;
-    public string guid => m_guid;
-    public Vector3 worldPosition => m_worldPosition;
-
-    public EntityState(string guid, Vector3 pos)
+    public class EntityState : KeyedObject<bool>
     {
-        m_guid = guid;
-        m_worldPosition = pos;
+        protected string m_guid;
+        protected Vector3 m_worldPosition;
+        public string guid => m_guid;
+        public Vector3 worldPosition => m_worldPosition;
+
+        public EntityState(string guid, Vector3 pos)
+        {
+            m_guid = guid;
+            m_worldPosition = pos;
+        }
+
+        public EntityState ShallowCopy()
+        {
+            return (EntityState)this.MemberwiseClone();
+        }
     }
 
-    public EntityState ShallowCopy()
+    public class Entity : MonoBehaviour, IEntity
     {
-        return (EntityState)this.MemberwiseClone();
-    }
-}
+        [SerializeField]
+        bool m_startAsDisable = false;
+        public bool startAsDisable => m_startAsDisable;
 
-public class Entity : MonoBehaviour, IEntity
-{
-    [SerializeField]
-    bool m_startAsDisable = false;
-    public bool startAsDisable => m_startAsDisable;
-
-    [SerializeField]
-    bool m_isInit = false;
-    public bool isInit => m_isInit;
+        [SerializeField]
+        bool m_isInit = false;
+        public bool isInit => m_isInit;
 
 
 
-    [SerializeField]
-    protected string m_name;
-    public new string name => string.IsNullOrEmpty(m_name) ? gameObject.name : m_name;
+        [SerializeField]
+        protected string m_name;
+        public new string name => string.IsNullOrEmpty(m_name) ? gameObject.name : m_name;
 
-    [SerializeField]
-    string m_category;
-    public string[] category { get; protected set; }
+        [SerializeField]
+        string m_category;
+        public string[] category { get; protected set; }
 
-    protected Vector3 m_originPosition;
+        protected Vector3 m_originPosition;
 
-    [SerializeField]
-    protected string m_GUID;
-    public string GUID => m_GUID;
+        [SerializeField]
+        protected string m_GUID;
+        public string GUID => m_GUID;
 
-    private void Awake()
-    {
-        m_originPosition = transform.position;
-        category = m_category.Split(',');
-    }
+        private void Awake()
+        {
+            m_originPosition = transform.position;
+            category = m_category.Split(',');
+        }
 
-    protected virtual void Start()
-    {
-        var id = string.IsNullOrEmpty(m_GUID) ? m_name : m_GUID;
-        //if (AllEntity.ContainsKey(id) && id.Equals("Player") == false)
-        //    ApplyEntityState(AllEntity[id]);
-        //else
-        Toggle(!m_startAsDisable);
+        protected virtual void Start()
+        {
+            var id = string.IsNullOrEmpty(m_GUID) ? m_name : m_GUID;
+            //if (AllEntity.ContainsKey(id) && id.Equals("Player") == false)
+            //    ApplyEntityState(AllEntity[id]);
+            //else
+            Toggle(!m_startAsDisable);
 
-        m_isInit = true;
-    }
+            m_isInit = true;
+        }
 
-    protected virtual void OnEnable()
-    {
-        if (m_isInit == false) return;
-    }
+        protected virtual void OnEnable()
+        {
+            if (m_isInit == false) return;
+        }
 
-    protected virtual void OnDisable()
-    {
-        if (m_isInit == false) return;
-    }
+        protected virtual void OnDisable()
+        {
+            if (m_isInit == false) return;
+        }
 
-    public virtual void Toggle(bool value)
-    {
-        gameObject.SetActive(value);
-        var id = string.IsNullOrEmpty(m_GUID) ? m_name : m_GUID;
+        public virtual void Toggle(bool value)
+        {
+            gameObject.SetActive(value);
+            var id = string.IsNullOrEmpty(m_GUID) ? m_name : m_GUID;
 
-        //if (AllEntity.ContainsKey(id))
-        //{
-        //    AllEntity[id] = value;
-        //}
-        //else
-        //{
-        //    Util.Log("Entity", "Toggle", string.Format("Track guid:{0} entity:{1}", m_GUID, name));
-        //    AllEntity.Add(id, new EntityState()
-        //    {
-        //        key = name,
-        //        value = value,
-        //        guid = id,
-        //        worldPosition = transform.position
-        //    });
-        //}
-    }
+            //if (AllEntity.ContainsKey(id))
+            //{
+            //    AllEntity[id] = value;
+            //}
+            //else
+            //{
+            //    Util.Log("Entity", "Toggle", string.Format("Track guid:{0} entity:{1}", m_GUID, name));
+            //    AllEntity.Add(id, new EntityState()
+            //    {
+            //        key = name,
+            //        value = value,
+            //        guid = id,
+            //        worldPosition = transform.position
+            //    });
+            //}
+        }
 
-    public virtual void ApplyGUID(string arg0)
-    {
-        m_GUID = arg0;
+        public virtual void ApplyGUID(string arg0)
+        {
+            m_GUID = arg0;
 #if UNITY_EDITOR
-        EditorUtility.SetDirty(this);
+            EditorUtility.SetDirty(this);
 #endif
+        }
+
+        public bool ContainCategory(string value)
+        {
+            try
+            {
+                return category.Contains(value);
+            }
+            catch (System.NullReferenceException)
+            {
+                return false;
+            }
+        }
+
+        public bool ContainCategory(string[] value)
+        {
+            try
+            {
+                return category.Contains(value);
+            }
+            catch (System.NullReferenceException)
+            {
+                return false;
+            }
+        }
     }
 
-    public bool ContainCategory(string value)
+
+    public interface IEntity
     {
-        try
-        {
-            return category.Contains(value);
-        }
-        catch (System.NullReferenceException)
-        {
-            return false;
-        }
+        GameObject gameObject { get; }
+
+        bool ContainCategory(string value);
+        bool ContainCategory(string[] value);
     }
-
-    public bool ContainCategory(string[] value)
-    {
-        try
-        {
-            return category.Contains(value);
-        }
-        catch (System.NullReferenceException)
-        {
-            return false;
-        }
-    }
-}
-
-
-public interface IEntity
-{
-    GameObject gameObject { get; }
-
-    bool ContainCategory(string value);
-    bool ContainCategory(string[] value);
 }
